d7de93e1aa6a454dd57d3c017722a76b69d0df83
Дмитрий Марченко <mdi500100@gmail.com>
2024-09-20 15:38:58 +0300

    class student created

--- Lab1/Program.cs
+++ Lab1/Program.cs
@@ -1,4 +1,28 @@
 ﻿public class Student
 {
+    private string _name;
+
+    public int Age { get; set; }
+
+    public Student (string name) {
+        _name = name;
+    }
+
+    public Student(string name, int age)
+    {
+        _name = name;
+        Age = age;
+    }
+
+    public string WriteInfo()
+    {
+        return "Студент " + _name + " имеет возраст " + Age.ToString() + " лет";
+    }
+
+    public void BecomeOlder()
+    {
+        Age += 1;
+    }
+
 
 }
