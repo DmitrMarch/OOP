1f171d3e593e81990be4e185f09e9d56f5194a93
Дмитрий Марченко <mdi500100@gmail.com>
2024-09-26 13:51:23 +0300

    namaesapce Lab1 has been changed to Lab2

--- Lab2/Program.cs
+++ Lab2/Program.cs
@@ -1,6 +1,9 @@
 ﻿using Lab2;
 
-Student st1 = new Student("Дмитрий", 19, new Game("a"), new Subject("a"));
+Game game = new Game("");
+Subject subject = new Subject("");
+
+Student st1 = new Student("Дмитрий", 19, game, subject);
 Console.WriteLine(st1.WriteInfo());
 st1.BecomeOlder();
 Console.WriteLine(st1.WriteInfo());
--- Lab2/Student.cs
+++ Lab2/Student.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Lab1
+namespace Lab2
 {
     class Student
     {
