0922a5b66bcaa2134725989d519f178f59b56a5d
Дмитрий Марченко <98233694+DmitrMarch@users.noreply.github.com>
2024-09-26 13:48:27 +0300

    modificator public has been added to new Classes

--- Lab2/Program.cs
+++ Lab2/Program.cs
@@ -1,1 +1,6 @@
-﻿Console.WriteLine("Lab2!");
+﻿using Lab2;
+
+Student st1 = new Student("Дмитрий", 19, new Game("a"), new Subject("a"));
+Console.WriteLine(st1.WriteInfo());
+st1.BecomeOlder();
+Console.WriteLine(st1.WriteInfo());
