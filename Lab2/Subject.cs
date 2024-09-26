2b41ecf96f0137f52918fb0e07941c4f079b2c7d
Дмитрий Марченко <mdi500100@gmail.com>
2024-09-26 13:29:52 +0300

    class Subject has been created

---
+++ Lab2/Subject.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2
+{
+    class Subject
+    {
+        private string _name;
+
+        Subject(string name)
+        {
+            _name = name;
+        }
+
+        public string GetSubjectName()
+        {
+            return _name;
+        }
+
+        public void SetSubjectName(string name)
+        {
+            _name = name;
+        }
+    }
+}
