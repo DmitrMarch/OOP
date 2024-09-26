305834616c4e8f1bff854d232dde18e38364a26b
Дмитрий Марченко <98233694+DmitrMarch@users.noreply.github.com>
2024-09-26 13:31:18 +0300

    class Game has been created

---
+++ Lab2/Game.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2
+{
+    class Game
+    {
+        string _name;
+
+        Game(string name)
+        {
+            _name = name;
+        }
+
+        public string GetGameName()
+        {
+            return _name;
+        }
+
+        public void SetGameName(string name)
+        {
+            _name = name;
+        }
+    }
+}
