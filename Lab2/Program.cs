6e8f0485a1393e5e67077db672c156ac95c1de47
Дмитрий Марченко <mdi500100@gmail.com>
2024-09-26 14:09:57 +0300

    lab2 has been finished

--- Lab2/Program.cs
+++ Lab2/Program.cs
@@ -1,9 +1,16 @@
 ﻿using Lab2;
 
-Game game = new Game("");
-Subject subject = new Subject("");
+Game game = new Game("Minecraft");
+Subject subject = new Subject("Мобильная разработка");
 
 Student st1 = new Student("Дмитрий", 19, game, subject);
 Console.WriteLine(st1.WriteInfo());
-st1.BecomeOlder();
-Console.WriteLine(st1.WriteInfo());
+//Console.WriteLine(st1._game); //"Student._game" недоступен из-за его уровня защиты
+Console.WriteLine(st1.Age);
+
+int number = 0;
+
+st1.GetNumber(number);
+Console.WriteLine(number);
+st1.RealGetNumber(ref number);
+Console.WriteLine(number);
