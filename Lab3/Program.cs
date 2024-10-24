321f39cb1dbdfbd61556b54c7c7cc4397e8fee79
Дмитрий Марченко <mdi500100@gmail.com>
2024-10-24 13:48:02 +0300

    more static

--- Lab3/Program.cs
+++ Lab3/Program.cs
@@ -1,1 +1,19 @@
-﻿Console.WriteLine("Lab3");
+﻿namespace Lab3
+{
+    class Program()
+    {
+        private static Student st1 = new("Дмитрий");
+
+        static void Main()
+        {
+            Console.WriteLine(st1.WriteInfo());
+            Student st2 = new("Никита");
+            Console.WriteLine(st2.WriteInfo());
+            Student st3 = new("Егор", 20);
+            Console.WriteLine(st3.WriteInfo());
+
+            Student.GroupToConsole();
+            Student.University.Love();
+        }
+    }
+}
