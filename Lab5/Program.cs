42a9f5c1c3cd57078c72a1bde5f7ee060c8170b8
Дмитрий Марченко <mdi500100@gmail.com>
2024-10-29 13:43:41 +0300

    new interfaces

--- Lab5/Program.cs
+++ Lab5/Program.cs
@@ -4,13 +4,8 @@
     {
         static void Main()
         {
-            ITStudent st1 = new("Никита");
-            st1.WriteStudentType();
-            st1.WritePersonType();
-            Student st2 = (Student)st1;
-            st2.WriteStudentType();
-            st2.WritePersonType();
-            Console.WriteLine(st1.ToString());
+            ITStudent st1 = new("Никита", new Subject("математика"));
+            Console.WriteLine(st1.WriteInfo());
         }
     }
 }
