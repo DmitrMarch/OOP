75c28aa73e1c578ca4baaa76315da4886554705f
Дмитрий Марченко <98233694+DmitrMarch@users.noreply.github.com>
2024-10-30 16:04:46 +0300

    Lab5 has been finished

--- Lab5/Program.cs
+++ Lab5/Program.cs
@@ -5,10 +5,16 @@
         static void Main()
         {
             ITStudent st1 = new("Никита", 20, new Subject("математика"));
+            ITStudent st2 = new("Владимир", 22, new Subject("SQL"));
             IPerson pst = st1;
             ISpecialist itst = st1;
             Console.WriteLine(pst.WriteInfo());
             Console.WriteLine(itst.WriteInfo());
+            Console.WriteLine(st1.CompareTo(st2));
+            Student st_clone = ((Student)st1.Clone());
+            st_clone.Age = 100;
+            Console.WriteLine(st1.Age);
+            Console.WriteLine(st_clone.Age);
         }
     }
 }
