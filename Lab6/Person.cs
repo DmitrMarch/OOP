b750dd3022b8c9ffc5cce94744302a266b3243ff
Дмитрий Марченко <98233694+DmitrMarch@users.noreply.github.com>
2024-11-01 15:38:01 +0300

    project Lab6 has been created

---
+++ Lab6/Person.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab6
+{
+    public abstract class Person
+    {
+        public abstract int Age { get; set; }
+
+        public abstract string WriteInfo();
+    }
+}
