b0f055aedce990cc41a2ba35503e43d550717228
Дмитрий Марченко <mdi500100@gmail.com>
2024-11-01 15:38:01 +0300

    project Lab6 has been created

---
+++ Lab6/ITStudent.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Lab6
+{
+    public class ITStudent : Student
+    {
+        public ITStudent(string name) : base("")
+        {
+            _name = name;
+        }
+
+        public ITStudent(string name, int age) : base("", 0)
+        {
+            _name = name;
+            Age = age;
+        }
+
+        public new void WritePersonType()
+        {
+            Console.WriteLine("Я" + " айти " + PersonType);
+        }
+
+        public override void WriteStudentType()
+        {
+            Console.WriteLine("Я" + " айти " + PersonType);
+        }
+    }
+}
---
+++ Lab6/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Lab6
+{
+    class Program()
+    {
+        static void Main()
+        {
+            ITStudent st1 = new("Никита");
+            st1.WriteStudentType();
+            st1.WritePersonType();
+            Student st2 = (Student)st1;
+            st2.WriteStudentType();
+            st2.WritePersonType();
+            Console.WriteLine(st1.ToString());
+        }
+    }
+}
