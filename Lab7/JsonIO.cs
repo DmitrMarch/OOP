3121e2eda28e395d48651f9d3f99cae35f7d59d2
Дмитрий Марченко <98233694+DmitrMarch@users.noreply.github.com>
2024-12-25 00:59:14 +0300

    handlers for the Add, Edit, and Delete buttons have been added to the form class, the Student class has been shortened, and the Person class has been slightly modified.

---
+++ Lab7/JsonIO.cs
@@ -0,0 +1,30 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab7
+{
+    public static class JsonIO
+    {
+        public static List<Student> readJSONArray(String path)
+        {
+            string students_json = File.ReadAllText(path);
+
+            List<Student> Students = JsonConvert.DeserializeObject
+                <List<Student>>(students_json)!;
+
+            return Students;
+        }
+
+        public static void writeJSONArray(String path, List<Student> students)
+        {
+            string students_json = JsonConvert.SerializeObject(students, 
+                Formatting.Indented);
+
+            File.WriteAllText(path, students_json);
+        }
+    }
+}
--- Lab7/Student.cs
+++ Lab7/Student.cs
@@ -6,85 +6,28 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Xml.Linq;
 
-namespace Lab6
+namespace Lab7
 {
     public class Student : Person
     {
-        private string _recordBook;
-        private string _department;
-        private string _specification;
-        private string _group;
-        private string _dateOfAdmission;
+        private List<Student> _students = new();
 
         public Student(string fullname, string recordBook, string department, 
             string specification, string group, string dateOfAdmission)
         : base("")
         {
-            this.Fullname = fullname;
-            this._recordBook = recordBook;
-            this._department = department;
-            this._specification = specification;
-            this._group = group;
-            this._dateOfAdmission = dateOfAdmission;
+            Fullname = fullname;
+            RecordBook = recordBook;
+            Department = department;
+            Specification = specification;
+            Group = group;
+            DateOfAdmission = dateOfAdmission;
         }
 
-        public string RecordBook 
-        {
-            get { 
-                return _recordBook;
-            }
-            set { 
-                _recordBook = value;
-            }
-        }
-
-        public string Department
-        {
-            get
-            {
-                return _department;
-            }
-            set
-            {
-                _department = value;
-            }
-        }
-
-        public string Specification
-        {
-            get
-            {
-                return _specification;
-            }
-            set
-            {
-                _specification = value;
-            }
-        }
-
-        public string Group
-        {
-            get
-            {
-                return _group;
-            }
-            set
-            {
-                _group = value;
-            }
-        }
-
-        public string DateOfAdmission
-        {
-            get
-            {
-                return _dateOfAdmission;
-            }
-            set
-            {
-                _dateOfAdmission = value;
-            }
-        }
-
+        public string RecordBook { get; set; }
+        public string Department { get; set; }
+        public string Specification { get; set; }
+        public string Group { get; set; }
+        public string DateOfAdmission { get; set; }
     }
 }
