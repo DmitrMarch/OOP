54a8ad2d81c8b1984c33a5972c92aed15033941e
Дмитрий Марченко <98233694+DmitrMarch@users.noreply.github.com>
2024-12-26 16:33:05 +0300

    fixed lab10, lab10 is finished

--- Lab10/Properties/Settings.Designer.cs
+++ Lab10/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace Lab10.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.9.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.11.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -26,8 +26,8 @@ namespace Lab10.Properties {
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
-        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=localhost\\SQLEXPRESS;Initial Catalog=films_db;Persist Security Info=Tr" +
-            "ue;User ID=sa;Password=123;TrustServerCertificate=True")]
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=localhost\\SQLEXPRESS;Initial Catalog=films_db;Persist Security Info=T" +
+            "rue;User ID=sa;Password=123;TrustServerCertificate=True")]
         public string films_dbConnectionString {
             get {
                 return ((string)(this["films_dbConnectionString"]));
